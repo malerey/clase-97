7ffa2744c30f3e18f46e0bf3b36f9380c83295df
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-04-13 22:04:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const MovieContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`
+
+const Movie = styled.article`
+  margin: 20px;
+  width: 180px;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+const Modal = styled.div`
+  width: 700px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 20px;
+  position: absolute;
+  right: 30%;
+  background-color: #fff;
+
+  img {
+    width: 180px;
+    height: auto;
+  }
+`
+
+const App = () => {
+
+  const [listaDePeliculas, setListaDePeliculas] = useState([])
+  const [modalAbierto, setModalAbierto] = useState(false)
+  const [peliculaElegida, setPeliculaElegida] = useState(null)
+  const [generos, setGeneros] = useState([])
+  const [generosPeliculaElegida, setGenerosPeliculaElegida] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=84e25ac11ad6125024e1d376337be05f`)
+    .then(res => res.json())
+    .then(data => setListaDePeliculas(data.results))
+  }, [])
+
+  const handleClick = pelicula => {
+    setPeliculaElegida(pelicula)
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=84e25ac11ad6125024e1d376337be05f`)
+    .then(res => res.json())
+    .then(data => setGeneros(data.genres))
+    handleClickModal()
+    setGenerosPeliculaElegida(generos.filter(genero => {
+      return peliculaElegida.genre_ids.includes(genero.id)
+    }))
+  }
+
+  const handleClickModal = () => {
+    setModalAbierto(!modalAbierto)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {modalAbierto && 
+        <Modal>
+          <button onClick={handleClickModal}>X</button>
+          <h3>{peliculaElegida.title}</h3>
+          <img src={`https://image.tmdb.org/t/p/w500${peliculaElegida.poster_path}`} />
+          <p>resumen: {peliculaElegida.overview}</p>
+          <p>fecha de lanzamiento: {new Date(peliculaElegida.release_date).toLocaleDateString('es')}</p>
+          <p>Generos: {generosPeliculaElegida.map(generos => generos.name).join(", ")}</p>
+        </Modal>
+      }
+      <MovieContainer>
+      {listaDePeliculas.map((pelicula) => (
+        <Movie key={pelicula.id} onClick={() => handleClick(pelicula)}>
+          <p>{pelicula.title}</p>
+          <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}/>
+        </Movie>
+        ))}
+      </MovieContainer>
+    </>
   );
 }
 
